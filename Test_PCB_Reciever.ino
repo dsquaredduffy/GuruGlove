c9ac9bfd2cb6815ec0b08c787ad61d497a67c4a0
mguil54 <mguil54@lsu.edu>
2016-10-31 17:15:59 -0500

    Receiver update

--- Test_PCB_Reciever.ino
+++ Test_PCB_Reciever.ino
@@ -1,6 +1,12 @@
 const char startOfNumberDelimiter = '<';
 const char endOfNumberDelimiter   = '>';
 
+enum { FlexDegree, AccPitch, AccRoll };
+
+int whichNumber = flex_deg;
+
+float flex_deg, acc_pitch, acc_roll;
+
 void setup ()
  { 
  Serial.begin (115200);
@@ -9,7 +15,28 @@ void setup ()
  
 void processNumber (const long n)
  {
- Serial.println (n);
+  switch (whichNumber)
+    {
+    case FlexDegree: 
+      flex_deg = x;
+      whichNumber = AccPitch;
+      Serial.print ("Flex Degree = ");
+      break;
+      
+    case AccPitch: 
+      acc_pitch = x;
+      whichNumber = AccRoll;
+      Serial.print ("Accelerometer Pitch = ");
+      break;
+
+    case AccRoll: 
+      acc_roll = x;
+      whichNumber = FlexDegree;
+      Serial.print ("Accelerometer Roll = ");
+      break;
+    }
+    
+  Serial.println (n);
  }
  
 void processInput ()
@@ -50,6 +77,5 @@ void loop ()
  
  if (Serial.available ())
    processInput ();
-   
- // do other stuff here
+      
  } 
