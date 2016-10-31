e50da45c431328e25cdcfeef3e335bcbfcb664c6
mguil54 <mguil54@lsu.edu>
2016-10-31 17:28:27 -0500

    Add files via upload

--- Test_PCB_Reciever.ino
+++ Test_PCB_Reciever.ino
@@ -1,43 +1,17 @@
+
 const char startOfNumberDelimiter = '<';
 const char endOfNumberDelimiter   = '>';
-
-enum { FlexDegree, AccPitch, AccRoll };
-
-int whichNumber = flex_deg;
-
-float flex_deg, acc_pitch, acc_roll;
 
 void setup ()
  { 
  Serial.begin (115200);
  Serial.println ("Starting ...");
- }
+ } // end of setup
  
 void processNumber (const long n)
  {
-  switch (whichNumber)
-    {
-    case FlexDegree: 
-      flex_deg = x;
-      whichNumber = AccPitch;
-      Serial.print ("Flex Degree = ");
-      break;
-      
-    case AccPitch: 
-      acc_pitch = x;
-      whichNumber = AccRoll;
-      Serial.print ("Accelerometer Pitch = ");
-      break;
-
-    case AccRoll: 
-      acc_roll = x;
-      whichNumber = FlexDegree;
-      Serial.print ("Accelerometer Roll = ");
-      break;
-    }
-    
-  Serial.println (n);
- }
+ Serial.println (n);
+ }  // end of processNumber
  
 void processInput ()
  {
@@ -48,34 +22,36 @@ void processInput ()
  
  switch (c)
    {
-     case endOfNumberDelimiter:  
-       if (negative) 
-         processNumber (- receivedNumber); 
-       else
-         processNumber (receivedNumber); 
-  
-     // fall through to start a new number
-     case startOfNumberDelimiter: 
-       receivedNumber = 0; 
-       negative = false;
-       break;
-       
-     case '0' ... '9': 
-       receivedNumber *= 10;
-       receivedNumber += c - '0';
-       break;
-       
-     case '-':
-       negative = true;
-       break;
+     
+   case endOfNumberDelimiter:  
+     if (negative) 
+       processNumber (- receivedNumber); 
+     else
+       processNumber (receivedNumber); 
+
+   // fall through to start a new number
+   case startOfNumberDelimiter: 
+     receivedNumber = 0; 
+     negative = false;
+     break;
+     
+   case '0' ... '9': 
+     receivedNumber *= 10;
+     receivedNumber += c - '0';
+     break;
+     
+   case '-':
+     negative = true;
+     break;
      
    } // end of switch  
- }  
+ }  // end of processInput
  
 void loop ()
  {
  
  if (Serial.available ())
    processInput ();
-      
- } 
+   
+ // do other stuff here
+ } // end of loop
