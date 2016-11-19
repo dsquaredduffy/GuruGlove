9c92e3a565403b89268fd74f60f54330a7d110bc
mguil54 <mguil54@lsu.edu>
2016-11-18 18:55:09 -0600

    Add files via upload

--- Final_Motor_Program.ino
+++ Final_Motor_Program.ino
@@ -20,22 +20,23 @@ const byte numChars = 100;
 char receivedChars[numChars]; // an array to store the received data
 
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(115200);
   Serial.flush();
   Serial.println ("WAITING FOR DATA ...");
   shoulder_lift_servo.attach(11);    
-  shoulder_rotate_servo.attach(9);   
-  elbow_servo.attach(13);  
+  shoulder_rotate_servo.attach(12);   
+  elbow_servo.attach(8);  
   wrist_rotation_servo.attach(9); 
-  wrist_flexion_servo.attach(8); 
-  finger_servo.attach(8);
+  wrist_flexion_servo.attach(10); 
+  finger_servo.attach(7);
+
 }
 
 void processDegrees (const long degree)
  {
   switch (whichNumber)
     {
-    case Shoulder_Pitch: 
+     case Shoulder_Pitch: 
       shoulder_pitch = degree;
       whichNumber = Shoulder_Yaw;
       shoulder_lift_servo.write(shoulder_pitch);
@@ -45,37 +46,37 @@ void processDegrees (const long degree)
     case Shoulder_Yaw: 
       shoulder_yaw = degree;
       whichNumber = Elbow_Flexion;
-      shoulder_rotate_servo.write(shoulder_yaw);
+      //shoulder_rotate_servo.write(shoulder_yaw);
       Serial.print ("Shoulder Yaw = ");
       break;
 
     case Elbow_Flexion: 
       elbow_flexion = degree;
       whichNumber = Wrist_Roll;
-      elbow_servo.write(elbow_flexion);
+      //elbow_servo.write(elbow_flexion);
       Serial.print ("Elbow Flexion Angle = ");
       break;
 
      case Wrist_Roll: 
       wrist_roll = degree;
       whichNumber = Wrist_Flexion;
-      wrist_rotation_servo.write(wrist_roll);
+      //wrist_rotation_servo.write(wrist_roll);
       Serial.print ("Wrist Roll = ");
       break;
       
     case Wrist_Flexion: 
       wrist_flexion = degree;
       whichNumber = Finger_Flexion;
-      wrist_flexion_servo.write(wrist_flexion);
-      Serial.print ("Wrist Flexion Angle = ");
+      //wrist_flexion_servo.write(wrist_flexion);
+      Serial.print ("Forward Wrist Motion = ");
       break;
 
     case Finger_Flexion: 
       finger_flexion = degree;
       whichNumber = Shoulder_Pitch;
-      finger_servo.write(finger_flexion);
+      //finger_servo.write(finger_flexion);
       Serial.print ("Finger Flexion Angle = ");
-      break;     
+      break; 
     }
     
   Serial.println (degree);
@@ -179,5 +180,5 @@ void loop ()
 {
    if (Serial.available ())
       processInput ();
-   Serial.flush();
+   //Serial.flush();
 }
